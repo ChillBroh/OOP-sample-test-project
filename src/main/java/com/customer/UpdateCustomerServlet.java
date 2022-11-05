913ea3bb8397df09a3f9eac4c9d4a99c3ae7f365
Ishara Madusanka <isharamadusanka410@gmail.com>
2022-11-06 00:40:43 +0530

    update customer servlet parameter catching

--- src/main/java/com/customer/UpdateCustomerServlet.java
+++ src/main/java/com/customer/UpdateCustomerServlet.java
@@ -7,17 +7,18 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-/**
- * Servlet implementation class UpdateCustomerServlet
- */
+
 public class UpdateCustomerServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		String id = request.getParameter("id");
+		String email = request.getParameter("email");
+		String phone = request.getParameter("phone");
+		String username = request.getParameter("username");
+		String password = request.getParameter("passsword");
+		
 	}
 
 }
